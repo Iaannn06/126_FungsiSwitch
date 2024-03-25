580a936c9dcb26c9e8d15228de6d09f0148e2ed6
Iaannn06 <alfianazad05@gmail.com>
2024-03-25 10:20:23 +0700

    masukkan menu

--- 126_fungsiSwitch/126_fungsiSwitch.cpp
+++ 126_fungsiSwitch/126_fungsiSwitch.cpp
@@ -29,3 +29,18 @@ int perkalian(int bil_1, int bil_2) {
 int pembagian(int bil_1, int bil_2) {
 	return bil_1 / bil_2;
 }
+int main()
+	{
+		int pilihan;
+
+do {
+	system("cls");
+	cout << "=====Menu=====" << endl;
+	cout << "1. Penjumlahan" << endl;
+	cout << "2. pengurangan" << endl;
+	cout << "3. Perkalian" << endl;
+	cout << "4. pembagian" << endl;
+	cout << "Masukkan menu yang ingin di pilih (1/2/3/4/5" << endl;
+	cin >> pilihan;
+}
+	}
